907ad13987125f7cb74cec83760f4f9344a0f0ed
Jesus <kepxumana1@gmail.com>
2022-12-06 13:01:05 +0100

    Initial pseudonym

--- JavaPsBenchmark/src/main/java/BenchmarkIdemix.java
+++ JavaPsBenchmark/src/main/java/BenchmarkIdemix.java
@@ -1,16 +1,17 @@
 import abce.net.URI;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.ibm.zurich.idmx.buildingBlock.pseudonym.PseudonymBuildingBlock;
+import com.ibm.zurich.idmx.buildingBlock.pseudonym.scopeExclusive.ScopeExclusivePseudonymBuildingBlock;
+import com.ibm.zurich.idmx.buildingBlock.rangeProof.RangeProofBuildingBlock;
+import com.ibm.zurich.idmx.buildingBlock.rangeProof.fourSq.FourSquaresRangeProofBuildingBlock;
 import com.ibm.zurich.idmx.buildingBlock.signature.SignatureBuildingBlock;
-import com.ibm.zurich.idmx.buildingBlock.signature.cl.ClPublicKeyWrapper;
 import com.ibm.zurich.idmx.buildingBlock.signature.cl.ClSignatureBuildingBlock;
 import com.ibm.zurich.idmx.buildingBlock.structural.reveal.RevealAttributeBuildingBlock;
 import com.ibm.zurich.idmx.buildingBlock.systemParameters.EcryptSystemParametersWrapper;
-import com.ibm.zurich.idmx.device.ExternalSecretsManagerImpl;
 import com.ibm.zurich.idmx.exception.ConfigurationException;
 import com.ibm.zurich.idmx.exception.ProofException;
 import com.ibm.zurich.idmx.guice.CryptoTestModule;
-import com.ibm.zurich.idmx.interfaces.device.ExternalSecretsManager;
 import com.ibm.zurich.idmx.interfaces.proofEngine.ZkDirector;
 import com.ibm.zurich.idmx.interfaces.signature.ListOfSignaturesAndAttributes;
 import com.ibm.zurich.idmx.interfaces.state.CarryOverStateIssuer;
@@ -19,31 +20,30 @@ import com.ibm.zurich.idmx.interfaces.state.IssuanceStateRecipient;
 import com.ibm.zurich.idmx.interfaces.util.BigInt;
 import com.ibm.zurich.idmx.interfaces.util.BigIntFactory;
 import com.ibm.zurich.idmx.interfaces.util.RandomGeneration;
-import com.ibm.zurich.idmx.interfaces.util.group.GroupFactory;
 import com.ibm.zurich.idmx.interfaces.zkModule.ZkModuleProver;
 import com.ibm.zurich.idmx.interfaces.zkModule.ZkModuleProverIssuance;
 import com.ibm.zurich.idmx.interfaces.zkModule.ZkModuleVerifier;
 import com.ibm.zurich.idmx.interfaces.zkModule.ZkModuleVerifierIssuance;
 import com.ibm.zurich.idmx.jaxb.wrapper.CredentialSpecificationWrapper;
-import eu.abc4trust.keyManager.KeyManager;
 import eu.abc4trust.xml.*;
 import utils.IdemixUtils;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.net.URISyntaxException;
 import java.util.*;
 
 public class BenchmarkIdemix {
 
-    private static final int numberOfTests=50;
+    private static final int numberOfTests=2;
     private static final String SYSTEM_PARAM_FILE= "system_params_2048.xml";
     private static final String KEY_PAIR_FILE_PREFIX="issuerKeyPair_";
     private static final String CRED_SPEC_FILE_PREFIX="credentialSpecification";
     private static final String USERNAME="BenchUser";
     private static final String IDENTIFIER_OF_ZK_MODULE ="bench";
-    private static int warmup=50;
-    private static int minNumberOfAttributes=1;
-    private static int maxNumberOfAttributes=10;
+    private static int warmup=1;
+    private static int minNumberOfAttributes=4;
+    private static int maxNumberOfAttributes=4;
 
 
     public static void main(String[] args) throws Exception{
@@ -55,7 +55,7 @@ public class BenchmarkIdemix {
         File outputFileIssuance=new File("BenchIdemixIssuance.txt");
         PrintWriter writerIssuance=new PrintWriter(outputFileIssuance);
         for(int i=minNumberOfAttributes; i<=maxNumberOfAttributes ; i++) {
-            List<Object> times=benchIssuacnce(i);
+            List<Object> times= benchIssuanceAndProof(i);
             warmup=3; //For first bench need high warmup value
             writeTest(writerIssuance,i,(List<Long>)times.get(0),"NoAttr");
             writeWholeFile(i,(Map<Integer,List<Long>>)times.get(1),"ZkProof");
@@ -66,7 +66,7 @@ public class BenchmarkIdemix {
 
     }
 
-    private static List<Object> benchIssuacnce(int nAttr) throws Exception {
+    private static List<Object> benchIssuanceAndProof(int nAttr) throws Exception {
         List<Long> issuanceTimes=new LinkedList<>();
         Map<Integer,List<Long>> zkProofTimes=new HashMap<>();
         for(int i=0;i<=nAttr;i++)
@@ -80,6 +80,7 @@ public class BenchmarkIdemix {
         KeyPair keyPair = IdemixUtils.getResource(KEY_PAIR_FILE_PREFIX+nAttr+".xml", KeyPair.class,  true);
         Injector injector = Guice.createInjector(new CryptoTestModule());
         SignatureBuildingBlock sigBB = injector.getInstance(ClSignatureBuildingBlock.class);
+
         BigIntFactory bigIntFactory = injector.getInstance(BigIntFactory.class);
         ZkDirector director = injector.getInstance(ZkDirector.class);
         RandomGeneration randomGeneration = injector.getInstance(RandomGeneration.class);
@@ -117,8 +118,8 @@ public class BenchmarkIdemix {
             System.out.println(ok);
             IssuanceStateRecipient stateRecipient = zkv.recoverIssuanceState();
             ListOfSignaturesAndAttributes sig = sigBB.extractSignature(null, stateRecipient);
-            for(int j=0;j<=nAttr;j++){
-                List<Long> zkTimes=benchZK(j,sig,keyPair.getPublicKey(),credSpecId,systemParameters,sigBB,revealBB,director);
+            for(int j=0;j<=0;j++){ //XXX Only hide attributes
+                List<Long> zkTimes=benchZK(j,sig,keyPair.getPublicKey(),credSpecId,systemParameters,sigBB,revealBB,director,injector);
                 if(i>=warmup){
                     zkProofTimes.get(j).add(zkTimes.get(0));
                     zkVerifyTimes.get(j).add(zkTimes.get(1));
@@ -132,7 +133,13 @@ public class BenchmarkIdemix {
         return times;
     }
 
-    private static List<Long> benchZK(int numberOfRevealedAttr,ListOfSignaturesAndAttributes sigAndAttr,PublicKey pk,BigInt credSpecId, SystemParameters systemParameters, SignatureBuildingBlock clSignatureBuildingBlock, RevealAttributeBuildingBlock revealBB, ZkDirector zkDirector) throws ProofException, ConfigurationException {
+    private static List<Long> benchZK(int numberOfRevealedAttr,ListOfSignaturesAndAttributes sigAndAttr,PublicKey pk,BigInt credSpecId, SystemParameters systemParameters, SignatureBuildingBlock clSignatureBuildingBlock, RevealAttributeBuildingBlock revealBB, ZkDirector zkDirector, Injector injector) throws ProofException, ConfigurationException, URISyntaxException {
+        RangeProofBuildingBlock rangeBB=injector.getInstance(FourSquaresRangeProofBuildingBlock.class);
+        PseudonymBuildingBlock pseudoBB=injector.getInstance(ScopeExclusivePseudonymBuildingBlock.class);
+        URI deviceUri=new URI("urn:device");
+        URI scope=new URI("urn:scope");
+        AbstractPseudonym pseudonym=pseudoBB.createPseudonym(systemParameters,null,deviceUri,USERNAME,scope);
+    //Always use last attribute for range/pseudonym proof
         List<Long> times=new LinkedList<>();
         List<Integer> revealed=new LinkedList<>();
         for(int i=0;i<numberOfRevealedAttr;i++){
@@ -143,7 +150,6 @@ public class BenchmarkIdemix {
         List<BigInt> attributes = sigAndAttr.attributes;
         int numberOfAttributes = attributes.size();
 
-        URI deviceUri = null;
         URI credentialUri = null;
 
         List<ZkModuleProver> modulesProver = new ArrayList<ZkModuleProver>();
@@ -158,6 +164,10 @@ public class BenchmarkIdemix {
                 clSignatureBuildingBlock.getZkModuleProverPresentation(systemParameters, null, pk,
                         IDENTIFIER_OF_ZK_MODULE, tok, attributes, credSpecId, deviceUri, USERNAME, credentialUri);
         modulesProver.add(zkp);
+
+        ZkModuleProver zkpseudo=pseudoBB.getZkModuleProver(systemParameters,null,IDENTIFIER_OF_ZK_MODULE,pseudonym,deviceUri,USERNAME,scope);
+        modulesProver.add(zkpseudo); //XXX Added
+
         ZkProof proof = zkDirector.buildProof(USERNAME, modulesProver, systemParameters);
         long finish=System.nanoTime();
         times.add(finish-start);
@@ -171,6 +181,10 @@ public class BenchmarkIdemix {
                 clSignatureBuildingBlock.getZkModuleVerifierPresentation(systemParameters, null, pk,
                         IDENTIFIER_OF_ZK_MODULE, credSpecId, numberOfAttributes, false);
         modulesVerifier.add(zkv);
+
+        ZkModuleVerifier zkpseudoverf=pseudoBB.getZkModuleVerifier(systemParameters,null,IDENTIFIER_OF_ZK_MODULE,scope,pseudoBB.getPseudonymAsBytes(pseudonym));
+        modulesVerifier.add(zkpseudoverf);
+
         boolean result = zkDirector.verifyProof(proof, modulesVerifier, systemParameters);
         finish=System.nanoTime();
         times.add(finish-start);
